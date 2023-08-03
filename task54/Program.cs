f748474ed370d79ba7c12593dd6a9bcbc8ba2cb6
Denis-Moiseev <mrtesla666@gmail.com>
2023-08-03 11:29:38 +0800

    finishTask54

---
+++ task54/Program.cs
@@ -0,0 +1,59 @@
+﻿int[,] ElementsDescendingOrderInRows(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1) - 1; j++)
+        {
+            for (int k = j + 1; k < array.GetLength(1); k++)
+            {
+                if (array[i, j] < array[i, k])
+                {
+                    int temp = array[i, j];
+                    array[i, j] = array[i, k];
+                    array[i, k] = temp;
+                }
+            }
+        }
+    }
+    return array;
+}
+
+void Print2DArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write($"{array[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+int[,] Create2DArray(int rows, int columns, int startValue, int finishValue)
+{
+    int[,] matrix = new int[rows, columns];
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = new Random().Next(startValue, finishValue + 1);
+        }
+    }
+    return matrix;
+}
+
+int GetInput(string text)
+{
+    Console.Write(text);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int rows = GetInput("Введите количество строк массива: ");
+int columns = GetInput("Введите количество столбцов массива: ");
+int[,] array = Create2DArray(rows, columns, 1, 9);
+Console.WriteLine("Задан массив: ");
+Print2DArray(array);
+int[,] modifiedArray = ElementsDescendingOrderInRows(array);
+Console.WriteLine("Измененный массив: ");
+Print2DArray(modifiedArray);
